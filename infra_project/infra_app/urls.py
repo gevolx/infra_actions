faa49b4dda1a1252ea09c7b5cf531b23db7fa191
Vladislav Gerasimov <Vladislav.Gerasimov@kaspersky.com>
2022-10-01 20:47:01 +0300

    Added build docker image in workflow

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -7,5 +7,5 @@ app_name = 'infra_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('second_page/', views.second_page, name='second_page'),
-
+    path('third_page/', views.second_page, name='third_page'),
 ]
